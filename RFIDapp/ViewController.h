51b8b31604553a4e11fe48dc0cedb37fb7d4161e
Brandon Smith <brandon.smith@yale.edu>
2013-12-01 12:25:20 -0500

    Initial Commit

---
+++ RFIDapp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RFIDapp
+//
+//  Created by Brandon Smith on 12/1/13.
+//  Copyright (c) 2013 Brandon Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RFIDappTests/RFIDappTests.m
@@ -0,0 +1,34 @@
+//
+//  RFIDappTests.m
+//  RFIDappTests
+//
+//  Created by Brandon Smith on 12/1/13.
+//  Copyright (c) 2013 Brandon Smith. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RFIDappTests : XCTestCase
+
+@end
+
+@implementation RFIDappTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
